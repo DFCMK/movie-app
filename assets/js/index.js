a1221a5d4712d929bdb9e96d0d66f03d381b5246
Denes <denes@pop-os.Home>
2025-05-08 17:22:01 +0100

    Added movies into Array as json, cause URL was not working

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,14 +10,95 @@ let filteredArrOfMovies = [];
 
 const URL = "https://movies-app.prakashsakari.repl.co/api/movies";
 
-const getMovies = async (url) => {
+/*const getMovies = async (url) => {
   try {
     const { data } = await axios.get(url);
     return data;
   } catch (err) {}
-};
+};*/
 
-let movies = await getMovies(URL);
+let movies = [
+  {
+    "id": 1,
+    "name": "The Shawshank Redemption",
+    "genre": "Drama",
+    "imdb_rating": 9.3,
+    "duration": 142,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"
+  },
+  {
+    "id": 2,
+    "name": "The Godfather",
+    "genre": "Crime, Drama",
+    "imdb_rating": 9.2,
+    "duration": 175,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+  },
+  {
+    "id": 3,
+    "name": "The Dark Knight",
+    "genre": "Action, Crime, Drama",
+    "imdb_rating": 9.0,
+    "duration": 152,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Pulp Fiction",
+    "genre": "Crime, Drama",
+    "imdb_rating": 8.9,
+    "duration": 154,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Fight Club",
+    "genre": "Drama",
+    "imdb_rating": 8.8,
+    "duration": 139,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Inception",
+    "genre": "Action, Adventure, Sci-Fi",
+    "imdb_rating": 8.8,
+    "duration": 148,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"
+  },
+  {
+    "id": 7,
+    "name": "The Matrix",
+    "genre": "Action, Sci-Fi",
+    "imdb_rating": 8.7,
+    "duration": 136,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+  },
+  {
+    "id": 8,
+    "name": "Goodfellas",
+    "genre": "Biography, Crime, Drama",
+    "imdb_rating": 8.7,
+    "duration": 146,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+  },
+  {
+    "id": 9,
+    "name": "The Silence of the Lambs",
+    "genre": "Crime, Drama, Thriller",
+    "imdb_rating": 8.6,
+    "duration": 118,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+  },
+  {
+    "id": 10,
+    "name": "Interstellar",
+    "genre": "Adventure, Drama, Sci-Fi",
+    "imdb_rating": 8.6,
+    "duration": 169,
+    "img_link": "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+  }
+];
 console.log(movies);
 
 const createElement = (element) => document.createElement(element);
@@ -95,3 +176,9 @@ const createMovieCard = (movies) => {
     parentElement.appendChild(cardContainer);
   }
 };
+
+function handleSearch() {}
+
+seachInput.addEventListener("keyup", handleSearch);
+
+createMovieCard(movies);
